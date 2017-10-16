f38361f2b02446c8a2616563c6b73f43038ae5a8
Roland Tecson <roland_tecson@hotmail.com>
2017-10-16 15:02:01 -0700

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Roland Tecson on 2017-10-16.
+//  Copyright © 2017 Roland Tecson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Roland Tecson on 2017-10-16.
+//  Copyright © 2017 Roland Tecson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
